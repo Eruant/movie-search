5b609244c06cb3ff71b53686ca0c1c87998615f2
Matt Gale <matt@littleball.co.uk>
2016-12-01 15:39:59 +0000

    Adding subscriptions and dispatcher to bus

--- shared/bus.js
+++ shared/bus.js
@@ -1,8 +1,23 @@
 const createBus = (defaultState = {}) => {
   const state = defaultState
 
+  const getState = () => state
+
+  const subscribe = () => {
+    const id = Math.random().toString(36).substring(12)
+    return id
+  }
+
+  const dispatch = (action) => {
+    if (!action || !action.type) {
+      throw new Error('dispatch requires an object with a type property')
+    }
+  }
+
   return {
-    getState: () => state
+    getState,
+    subscribe,
+    dispatch
   }
 }
 
--- shared/bus.test.js
+++ shared/bus.test.js
@@ -23,3 +23,29 @@ tap.test('bus: create with a starting state', test => {
   test.equal(state.foo, 'bar', 'state should match value passed in')
   test.end()
 })
+
+tap.test('bus: subscriptions', test => {
+  const bus = createBus()
+  const subscription1 = bus.subscribe(() => {})
+  const subscription2 = bus.subscribe(() => {})
+
+  test.equal(/[a-z0-9]{12}/.test(subscription1), true, 'should return a valid id')
+  test.equal(subscription1 !== subscription2, true, 'subscription ids should be unique')
+
+  test.end()
+})
+
+tap.test('bus: dispatching messages', test => {
+  const bus = createBus()
+  const action = 'bad action'
+  const message = 'dispatch requires an object with a property of type set'
+
+  try {
+    bus.dispatch(action)
+    test.fail(message)
+  } catch (error) {
+    test.equal(error.message, 'dispatch requires an object with a type property', message)
+  }
+
+  test.end()
+})
