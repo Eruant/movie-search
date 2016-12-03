44de80638130d36a504ed3899912a18da36f6ee1
Matt Gale <matt@littleball.co.uk>
2016-12-03 14:36:35 +0000

    Added search fetch requests

--- shared/search.js
+++ shared/search.js
@@ -1,12 +1,35 @@
+const fetch = require('isomorphic-fetch')
+
 const isRequired = require('./isRequired')
 const dataStore = require('./dataStore')
 const {
-  searchRequest
+  searchRequest,
+  searchFailure,
+  searchSuccess
 } = require('./actions')
 
-module.exports = (value = isRequired({
+module.exports = (query = isRequired({
   category: 'search',
-  property: 'value'
+  property: 'query'
 })) => {
   dataStore.dispatch(searchRequest())
+
+  const queryString = [
+    `api_key=${process.env.MOVIE_DB_API_KEY}`,
+    'language=en-UK',
+    `query=${query}`,
+    'page=1',
+    'include_adult=false'
+  ].join('&')
+
+  fetch(`https://api.themoviedb.org?${queryString}`)
+    .then(response => {
+      return {fake: 'response'}
+    })
+    .then(data => {
+      dataStore.dispatch(searchSuccess(data))
+    })
+    .catch(error => {
+      dataStore.dispatch(searchFailure(error))
+    })
 }
--- shared/search.test.js
+++ shared/search.test.js
@@ -1,4 +1,6 @@
 const tap = require('tap')
+const nock = require('nock')
+
 const dataStore = require('./dataStore')
 const search = require('./search')
 
@@ -7,19 +9,39 @@ tap.test('search', test => {
     search()
     test.fail('Search requires a value to be supplied')
   } catch (error) {
-    test.equal(error.message, '[search] The property "value" is required')
+    test.equal(error.message, '[search] The property "query" is required')
   }
 
   test.end()
 })
 
-tap.test('search: dataStore updates', test => {
+tap.test('search: dataStore updates (success)', test => {
+  dataStore.reset()
+
+  nock('api.themoviedb.org')
+    .get('/3/search/movie')
+    .reply(200)
+
+  let counter = 0
+
   dataStore.subscribe(() => {
-    test.pass('searching should trigger an action to be dispatched')
-    test.end()
+    const state = dataStore.getState()
+
+    switch (counter) {
+      case 0:
+        test.equal(state.search.isLoading, true, 'should start loading')
+        break
+      case 1:
+        test.equal(state.search.isLoading, false, 'should complete loading')
+        test.equal(state.search.data !== null, true, 'should set some data')
+
+        nock.cleanAll()
+        test.end()
+        break
+    }
+
+    counter++
   })
 
   search('fantastic')
 })
-
-// TODO test that all actions are dispatched
